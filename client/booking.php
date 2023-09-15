e724a731cbd9d5706ff108627f509d827f77c851
Hemant7771 <hemantborn82@gmail.com>
2023-09-15 15:01:57 +0530

    title_updated

--- client/booking.php
+++ client/booking.php
@@ -10,7 +10,7 @@ if(!$_SESSION["username"]){
 
 <head>
     <meta charset="utf-8">
-    <title>Appetite - Bootstrap Restaurant Template</title>
+    <title>Appetite</title>
     <meta content="width=device-width, initial-scale=1.0" name="viewport">
     <meta content="" name="keywords">
     <meta content="" name="description">
