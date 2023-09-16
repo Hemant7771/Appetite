8bdd8b2df9f7a2f8625d793eb65de4fc97ec2f47
Sahil-Karnaje <sahilkaranje58@gmail.com>
2023-09-16 10:57:57 +0530

    booking validation half done

--- server/booking.php
+++ server/booking.php
@@ -2,22 +2,29 @@
 
 include 'db.php';
 
-if(isset($_POST["book_now"])){
+if (isset($_POST["book_now"])) {
     $name = $_POST["name"];
     $email = $_POST["email"];
     $date = $_POST["date"];
     $nop = $_POST["no_of_people"];
     $time = $_POST["time"];
 
-    $q = "INSERT INTO `booking` (`name`, `email`, `date`, `time`, `no_of_people`) VALUES ('$name','$email','$date','$time','$nop')";
+    // Check if a booking already exists for the same date and time
+    $selectQuery = "SELECT * FROM `booking` WHERE `date` = '$date' AND `time` = '$time'";
+    $data = mysqli_query($conn, $selectQuery);
 
-    $res = mysqli_query($conn, $q);
+    if (mysqli_num_rows($data) > 0) {
+        echo "<script>alert('Table already booked for this date and time!')</script>";
+    } else {
+        // If no existing booking found, insert the new booking
+        $q = "INSERT INTO `booking` (`name`, `email`, `date`, `time`, `no_of_people`) VALUES ('$name','$email','$date','$time','$nop')";
+        $res = mysqli_query($conn, $q);
 
-    if($res){
-        echo "<script>alert('Table booked successfully !')</script>";
-    }else{
-        echo "<script>alert('Something went wrong !')</script>";
-
+        if ($res) {
+            echo "<script>alert('Table booked successfully!')</script>";
+        } else {
+            echo "<script>alert('Something went wrong!')</script>";
+        }
     }
 }
 
